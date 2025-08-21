a934a9bf2e3d90f879890c101f006f9566e2736d
karim <kmlkt@bk.ru>
2025-08-21 15:55:15 +0500

    declarative package management

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
+
+alias zed='zeditor'
+alias nano='zed -n'
+alias vi='zed -n'
+export EDITOR='zeditor -n'
+export PATH="$PATH:~/.config/scripts"
---
+++ scripts/sync.sh
@@ -0,0 +1,32 @@
+RED='\e[31m'
+GREEN='\e[32m'
+BLUE='\e[34m'
+NC='\e[0m'
+
+PKG_FILE=~/.config/packages.txt
+INSTALL=$(comm -13 <(yay -Qqe | sort) <(sort $PKG_FILE))
+REMOVE=$(comm -23 <(yay -Qqe | sort) <(sort $PKG_FILE))
+
+if [[ -z "$INSTALL" && -z "$REMOVE" ]]; then
+echo "Nothing to do"
+exit
+fi
+
+echo "To install:"
+echo -e $GREEN
+echo $INSTALL
+echo -e $NC
+
+echo "To remove:"
+echo -e $RED
+echo $REMOVE
+echo -e $NC
+
+
+read -p "Proceed? (Y/n)" yn
+case $yn in
+    [Nn]* ) exit;;
+esac
+echo "Okay, let's do it"
+yay -S --noconfirm $INSTALL
+yay -Rns --noconfirm $REMOVE
