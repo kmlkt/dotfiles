035b20572782b6367d226ed432867269352acf62
karim <kmlkt@bk.ru>
2025-08-22 16:00:18 +0500

    auto commit

--- scripts/configure-bash.sh
+++ scripts/configure-bash.sh
@@ -6,6 +6,6 @@ alias zed='zeditor'
 alias nano='zed -n'
 alias vi='zed -n'
 export EDITOR='zeditor -n'
-export PATH="$PATH:~/.config/scripts"
 
+export PATH="$PATH:~/.config/scripts"
 alias s='sync.sh'
